9ad613c42c4b6f5e2a42b6b1106cdd9cf0b12577
AntonioAscot <antonioascot@protonmail.com>
2019-08-03 12:18:08 -0300

    New Javascript

---
+++ evaluator.js
@@ -0,0 +1,56 @@
+var players = {
+	playerName: [],
+	accuracyShortPasses: [],
+	accuracyLongPasses: [],
+	accuracyTakeOns: [],
+	helpMove: [],
+	changeDirection: [], 
+	shieldAndResis: [],
+	ballHandle: [],
+	playerId: []
+};
+
+var coachs = {
+	coachName: [],
+	possessionPorcentageTeam: [],
+	coachId: []
+	
+};
+
+var listElement = document.querySelector('#playerList ul');
+
+var counting = 0;
+function addPlayerPercentageStats()
+{//also add the name info
+	
+	players.playerName[counting] = document.getElementById("playerName").value;
+	players.accuracyShortPasses[counting] = document.getElementById("accuracyShortPasses").value;
+	players.accuracyLongPasses[counting] = document.getElementById("accuracyLongPasses").value;
+	players.accuracyTakeOns[counting] = document.getElementById("accuracyTakeOns").value;
+	counting++;
+	
+	
+}
+
+function addPlayerRadioStats()
+{
+	for (var i = 0; i < 4; i++)
+	{
+		if (i == 0)
+		{
+			
+		} 
+		if (i == 1)
+		{
+			
+		}
+		if (i == 2)
+		{
+			
+		}
+		if (i == 4)
+		{
+			
+		}
+	}
+}
