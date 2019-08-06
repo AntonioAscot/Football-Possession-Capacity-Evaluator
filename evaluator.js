ff7ab041418b64034eabacdc4de7cc24da678d8f
AntonioAscot <antonioascot@protonmail.com>
2019-08-05 23:17:16 -0300

    Update JavaScript

--- evaluator.js
+++ evaluator.js
@@ -18,18 +18,28 @@ var coachs = {
 };
 
 var listElement = document.querySelector('#playerList ul');
-
-var counting = 0;
+var countingCoach = 0;
+var countingPlayer = 0;
 function addPlayerPercentageStats()
 {//also add the name info
 	
+<<<<<<< HEAD
+	players.playerName[countingPlayer] = document.getElementById("playerName").value;
+	players.accuracyShortPasses[countingPlayer] = document.getElementById("accuracyShortPasses").value;
+	players.accuracyLongPasses[countingPlayer] = document.getElementById("accuracyLongPasses").value;
+	players.accuracyTakeOns[countingPlayer] = document.getElementById("accuracyTakeOns").value;
+	players.playerId[countingPlayer] = countingPlayer;
+	
+
+	counting++;
+=======
 	players.playerName[counting] = document.getElementById("playerName").value;
 	players.accuracyShortPasses[counting] = document.getElementById("accuracyShortPasses").value;
 	players.accuracyLongPasses[counting] = document.getElementById("accuracyLongPasses").value;
 	players.accuracyTakeOns[counting] = document.getElementById("accuracyTakeOns").value;
 	counting++;
 	
-	
+>>>>>>> 0e5d1dc1e742de860c7ce050315aa19096575476
 }
 
 function addPlayerRadioStats()
@@ -53,4 +63,13 @@ function addPlayerRadioStats()
 			
 		}
 	}
+}
+
+function addCoachStats()
+{
+	coachs.coachName[countingCoach] = document.getElementById("coachName").value;
+	coachs.possessionPorcentageTeam[countingCoach] = document.getElementById("possessionPorcentageTeam").value;
+	coachs.coachId[countingCoach] = countingCoach;
+
+	countingCoach++;
 }
